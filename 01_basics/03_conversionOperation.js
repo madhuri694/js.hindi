17704548b386f404257d0038bd293241a7b5ecf7
madhuri694 <bishtmadhuri21@gmail.com>
2025-09-01 06:50:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+//let score = "33abc" //check the typeof before printing as it might also print nan which is not a number 
+//let score = null
+
+//let score = undefined
+// let score = true
+//let score = "madhuri"
+//"33" => 33
+//"33abc" =>NaN
+// true => 1; false => 0
+let isLoggedIn = "madhuri"
+
+
+let booleanisLoggedIn = Boolean(isLoggedIn) 
+console.log(booleanisLoggedIn);
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score) //but if you change your valueInNumber then also check that number which shoing in string
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//1 => true; 0 => false
+// "" => false
+// "madhuri" => true
+
+let someNumber = 33 // giving some value to  the number
+let stringNumber = String(someNumber) //for changing a number into a string datatype
+console.log(stringNumber); // for printing
+console.log(typeof stringNumber); //to check if it really convert into strings or not 
+
+
+
